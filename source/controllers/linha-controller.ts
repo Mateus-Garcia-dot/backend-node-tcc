3a9e4993e89bd8887c9b258aabcf92e3ad3acb76
Fernanda Moraes Costa <fer-mcosta@hotmail.com>
2023-04-25 11:55:31 -0300

    Ajustes e removidos controllers inutilizados

--- source/controllers/linha-controller.ts
+++ source/controllers/linha-controller.ts
@@ -1,24 +1,16 @@
 import express from "express";
+import { Linha } from "../models/linha";
 import { Shape } from "../models/shape";
 import { LinhasService } from '../services/linha-service';
 
 
 export default class LinhasController {
 
-
     static buscarLinhas = async (req: express.Request, res: express.Response) => {
-        // const result = LinhasService.buscarLinhas();
-        // let linhas: [Linha] = result.data;
-        return res.status(200).json({
-        });
+        const result = await new LinhasService().buscarLinhas();
+        let linhas: Linha[] = result.data;
+        return res.status(200).json({ linhas });
     };
-
-    static buscarPontos = async (req: express.Request, res: express.Response) => {
-        // let pontos: [Ponto] = result.data;
-        return res.status(200).json({
-        });
-    };
-
 
     static buscarShapes = async (req: express.Request, res: express.Response) => {
         const linhaId: number = Number(req.params.linhaId);
--- source/services/linha-service.ts
+++ source/services/linha-service.ts
@@ -3,9 +3,8 @@ import axios from "axios";
 export class LinhasService {
 
     async buscarLinhas() {
-
+        return axios({ method: "get", url: "https://transporteservico.urbs.curitiba.pr.gov.br/getLinhas.php?c=d2fde" })
     }
-
 
     async buscarShape(linhaId: number) {
         return axios({ method: "get", url: "https://transporteservico.urbs.curitiba.pr.gov.br/getShapeLinha.php?linha=022&c=98ad8" })
