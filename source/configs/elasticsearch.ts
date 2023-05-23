78f3f25c38de913d9abec1e3cf921153adacd805
Fernanda Moraes Costa <ferchan@hotmail.com.br>
2023-05-22 23:30:00 -0300

    refactor: removido imports inutilizados

--- source/configs/elasticsearch.ts
+++ source/configs/elasticsearch.ts
@@ -1,10 +1,11 @@
 import elasticsearch from '@elastic/elasticsearch';
-import { trace } from '../routes/login-route';
 
+// @TODO: Extrair pro enviroments 
 function getClient() {
   const client = new elasticsearch.Client({
     node: 'https://elastic.tccurbstads.com/',
     auth: {username: "elastic", password: "!@ContaElastic"},
+    
   });
 
   return client;
