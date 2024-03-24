b016820f06c189f8fe9cbb6be29f15bee5cd4240
Mateus <mateus.medeiros@ufpr.br>
2024-03-24 16:55:30 -0300

    Revert "cleaner way to cache"
    This reverts commit b29079967e14b8982d444cb504dce0ce73181b45.

--- app/databases/redis_connection.py
+++ app/databases/redis_connection.py
@@ -3,19 +3,15 @@ import os
 
 class Redis:
     def __init__(self):
-        self.__client = redis.StrictRedis(host=os.getenv('REDIS_URL'), port=os.getenv('REDIS_PORT'), db=0, password=os.getenv('REDIS_PASSWORD'))
+        self.client = redis.StrictRedis(host=os.getenv('REDIS_URL'), port=os.getenv('REDIS_PORT'), db=0, password=os.getenv('REDIS_PASSWORD'))
 
     def set(self, key, value, ex=None):
-        self.__client.set(key, value, ex=ex)
+        self.client.set(key, value, ex=ex)
 
     def get(self, key):
-        return self.__client.get(key)
+        return self.client.get(key)
 
     def delete(self, key):
-        self.__client.delete(key)
-
-    @property
-    def client(self):
-        return self.__client
+        self.client.delete(key)
 
 redis_client = Redis()
