4f6fd73cdea724a806a29bfe7d27e2ae6ca7bcb4
Mateus <mateus.medeiros@ufpr.br>
2024-04-22 20:23:16 -0300

    cors fix

--- app/app.py
+++ app/app.py
@@ -1,8 +1,19 @@
 from fastapi import Depends, FastAPI
 from app.auth.jwt_auth import get_current_user
 from app.routes import users, lines
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(users.router)
 app.include_router(lines.router)
